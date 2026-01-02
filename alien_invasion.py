329abdeb68f6faaaa73a71b546f20a8f31cea0b2
Ezatullah Farazi <ezat@crimson.eecs.yorku.ca>
2026-01-02 05:12:30 -0500

    enhances the ship movements and set the speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,6 +29,7 @@ class AlienInvasion:
         """Start the game loop."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -37,6 +38,20 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+                    # Move the ship to the right.
+                    self.ship.rect.x += 1
+
 
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen."""
