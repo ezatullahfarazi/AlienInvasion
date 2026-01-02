5e0d602792f1775c2bde65697da4a459d1bcb4b0
Ezatullah Farazi <ezat@crimson.eecs.yorku.ca>
2026-01-02 07:41:24 -0500

    Bullet class and bullet  were defined

---
+++ bullet.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship."""
+
+    def __init__(self, ai_game):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
